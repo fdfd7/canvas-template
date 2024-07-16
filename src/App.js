14a1add8e62665406d8b95e384a4033d76810433
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2024-07-16 23:26:09 +0200

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import {drawPicture} from "./canvas";
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+function draw() {
+    // fuege deinen code hier hinzu
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    draw();
+})
